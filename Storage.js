67733926aebb5b7d07f7a9e5b2ba73844a5ee28c
Sidem <7662187+Sidem@users.noreply.github.com>
2024-03-06 16:22:24 +0100

    update Storage

--- Storage.js
+++ Storage.js
@@ -1,23 +1,6 @@
-let workouts = JSON.parse(localStorage.getItem('workouts')) || {};
-
-let availableWeights = JSON.parse(localStorage.getItem('availableWeights')) || {
-    b1000: { weight: 10, quantity: 1, name: "Barbell" },
-    b750: { weight: 7.5, quantity: 1, name: "Barbell" },
-    ezb650: { weight: 6.5, quantity: 1, name: "EZBar" },
-    d250: { weight: 2.5, quantity: 2, name: "Dumbbell" },
-    d200: { weight: 2, quantity: 4, name: "Dumbbell" },
-    kbh250: { weight: 2.5, quantity: 1, name: "KettlebellHandle" },
-    w2000: { weight: 20, quantity: 6, name: "Weight" },
-    w1000: { weight: 10, quantity: 4, name: "Weight" },
-    w500: { weight: 5, quantity: 12, name: "Weight" },
-    w250: { weight: 2.5, quantity: 8, name: "Weight" },
-    w200: { weight: 2, quantity: 4, name: "Weight" },
-    w125: { weight: 1.25, quantity: 4, name: "Weight" },
-    w100: { weight: 1, quantity: 8, name: "Weight" },
-    r25: { weight: 0.25, quantity: 16, name: "Ring" }
-};
-
-localStorage.setItem('availableWeights', JSON.stringify(availableWeights));
+let workouts = getWorkouts();
+let availableWeights = getAvailableWeights();
+setAvailableWeights(availableWeights);
 
 function getRecords() {
     return JSON.parse(localStorage.getItem('records')) || {};
@@ -28,7 +11,22 @@ function getWorkouts() {
 }
 
 function getAvailableWeights() {
-    return JSON.parse(localStorage.getItem('availableWeights')) || {};
+    return {
+        b1000: { weight: 10, quantity: 1, name: "Barbell" },
+        b750: { weight: 7.5, quantity: 1, name: "Barbell" },
+        ezb650: { weight: 6.5, quantity: 1, name: "EZBar" },
+        d250: { weight: 2.5, quantity: 2, name: "Dumbbell" },
+        d200: { weight: 2, quantity: 4, name: "Dumbbell" },
+        kbh250: { weight: 2.5, quantity: 1, name: "KettlebellHandle" },
+        w2000: { weight: 20, quantity: 6, name: "Weight" },
+        w1000: { weight: 10, quantity: 4, name: "Weight" },
+        w500: { weight: 5, quantity: 12, name: "Weight" },
+        w250: { weight: 2.5, quantity: 8, name: "Weight" },
+        w200: { weight: 2, quantity: 4, name: "Weight" },
+        w125: { weight: 1.25, quantity: 4, name: "Weight" },
+        w100: { weight: 1, quantity: 8, name: "Weight" },
+        r25: { weight: 0.25, quantity: 16, name: "Ring" }
+    };
 }
 
 function setRecords(records) {
